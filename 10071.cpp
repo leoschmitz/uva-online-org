b8312ee73f7d1e765464c32220d683231829ee29
Leonardo Schmitz <laschmitz@gmail.com>
2016-02-09 19:12:05 -0200

    problem 10071 solved

---
+++ 10071.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a(0), b(0), c(0);
+
+	while (cin >> a >> b)
+	{
+		c = 2 * a * b;
+
+		cout << c << endl;
+	}
+}
